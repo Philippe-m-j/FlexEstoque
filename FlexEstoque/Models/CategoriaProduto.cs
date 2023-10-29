084378515310bc11e0f77106b36e93497964980d
Philippe-mj <pin_moreira@hotmail.com>
2023-10-29 11:31:49 -0300

    Alteração Nomes Tabelas

--- FlexEstoque/Models/CategoriaProduto.cs
+++ FlexEstoque/Models/CategoriaProduto.cs
@@ -1,8 +1,11 @@
-﻿namespace FlexEstoque.Models
+﻿using System.ComponentModel;
+
+namespace FlexEstoque.Models
 {
     public class CategoriaProduto
     {
-        public int Id { get; set; } 
+        public int Id { get; set; }
+        [DisplayName("Nome")]
         public string Descricao { get; set; }
     }
 }
